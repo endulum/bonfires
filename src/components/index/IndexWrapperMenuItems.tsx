24b3b14d9bada8119a2f7d0afc3cba411e574d4f
endulum <71853916+endulum@users.noreply.github.com>
2024-03-31 14:18:15 -0700

    add About Bonfires blurb in index wrapper menu items

--- src/components/index/IndexWrapperMenuItems.tsx
+++ src/components/index/IndexWrapperMenuItems.tsx
@@ -1,7 +1,9 @@
 import { type NavigateFunction } from 'react-router-dom'
 import APIForm from '../APIForm.tsx'
 import { type IUserData, type MenuItems } from '../../types.ts'
+
 import GearSvg from '../../assets/icons/gear-solid.svg?react'
+import QuestionSvg from '../../assets/icons/circle-question-solid.svg?react'
 import LogoutSvg from '../../assets/icons/right-from-bracket-solid.svg?react'
 
 export default function IndexWrapperMenuItems (
@@ -43,6 +45,38 @@ export default function IndexWrapperMenuItems (
 
           <button type="submit" className="button">Submit</button>
         </APIForm>
+      )
+    }, {
+      title: 'About Bonfires',
+      icon: (<QuestionSvg />),
+      element: (
+        <>
+          <h2>About Bonfires</h2>
+          <p className="text-paragraph">
+            <b>Bonfires</b>
+            {' '}
+            is a simple web app allowing users to communicate with one another through camps.
+          </p>
+          <p className="text-paragraph">
+            A
+            {' '}
+            <b>camp</b>
+            {' '}
+            is a space where users send messages.
+            {' '}
+            These messages can be seen by all members of the camp.
+          </p>
+          <p className="text-paragraph">
+            Each camp has a
+            {' '}
+            <b>firestarter</b>
+            , or an admin, who has special powers over the camp.
+            {' '}
+            Whoever started the camp is automatically its firestarter.
+            {' '}
+            The firestarter is free to pass their role to another user, to promote them.
+          </p>
+        </>
       )
     }, {
       title: 'Log Out',
