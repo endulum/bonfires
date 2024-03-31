54db9305510ab61bef968a6d53ed93413f7862a8
endulum <71853916+endulum@users.noreply.github.com>
2024-03-30 21:24:51 -0700

    add CustomModal component and useOutsideClick to close on outside click

--- src/components/IndexWrapper.tsx
+++ src/components/IndexWrapper.tsx
@@ -1,17 +1,23 @@
 import { Outlet } from 'react-router-dom'
+import { useState } from 'react'
+import CustomModal from './CustomModal.tsx'
 import { type IUserData } from '../types.ts'
 
 import UserGearSvg from '../assets/icons/user-gear-solid.svg?react'
+import InfoParagraph from './InfoParagraph.tsx'
 
 export default function IndexWrapper ({ userData, logOut }: {
   userData: IUserData
   logOut: () => void
 }): JSX.Element {
+  const [modalOpen, setModalOpen] = useState<boolean>(false)
+  function toggleModal (): void { setModalOpen(!modalOpen) }
+
   return (
     <>
       <header>
         <h1>Bonfires</h1>
-        <button type="button" className="button" aria-label="User Settings">
+        <button type="button" className="button" aria-label="User Settings" onClick={toggleModal}>
           <UserGearSvg className="button-svg" />
           {userData.username}
         </button>
@@ -19,6 +25,16 @@ export default function IndexWrapper ({ userData, logOut }: {
       <main>
         <Outlet context={userData} />
       </main>
+
+      <CustomModal
+        contentLabel="test"
+        isOpen={modalOpen}
+        closeModal={toggleModal}
+      >
+        <InfoParagraph type="warning">
+          content
+        </InfoParagraph>
+      </CustomModal>
     </>
   )
 }
