ff03f459c4a889c0c51921f8248cf047d8e8a5e7
endulum <71853916+endulum@users.noreply.github.com>
2025-02-26 22:53:54 -0800

    have active user strip use display names when possible

--- src/components/unique/messages/ActiveUsers.tsx
+++ src/components/unique/messages/ActiveUsers.tsx
@@ -4,9 +4,12 @@ import { useActiveUsers } from "../../../hooks/useActiveUsers";
 import { useLogger } from "../../../hooks/useLogger";
 import { useOutletContext } from "react-router-dom";
 import { Fragment } from "react/jsx-runtime";
+import { useContext } from "react";
+import { ChannelContext } from "../channel-view/ChannelContext";
 
 export function ActiveUsers() {
-  const { user } = useOutletContext<{ user: { _id: string } }>();
+  const { user: you } = useOutletContext<{ user: { _id: string } }>();
+  const { getSettingsForUser } = useContext(ChannelContext);
   const activeUsers = useActiveUsers();
 
   useLogger({ activeUsers });
@@ -14,28 +17,31 @@ export function ActiveUsers() {
   return (
     <div className="activeroll flex-row align-start w100 g-05">
       {activeUsers
-        .filter((u) => u.user._id !== user._id)
-        .map((u) => (
-          <Fragment key={u.user._id}>
-            <div
-              className="activeroll-user flex-row g-25"
-              data-tooltip-id={`active_${u.user._id}`}
-              data-tooltip-content={`${u.user.username} is ${
-                u.isTyping ? "typing" : "active"
-              }`}
-              data-tooltip-place="top-start"
-            >
-              <img
-                src={`${import.meta.env.VITE_API_URL}/user/${
-                  u.user._id
-                }/avatar`}
-                alt={`Avatar of user ${u.user.username}`}
-              />
-              {u.isTyping && <small>is typing...</small>}
-            </div>
-            <Tooltip id={`active_${u.user._id}`} />
-          </Fragment>
-        ))}
+        .filter((u) => u.user._id !== you._id)
+        .map((u) => {
+          const user = getSettingsForUser(u.user._id);
+          return (
+            <Fragment key={u.user._id}>
+              <div
+                className="activeroll-user flex-row g-25"
+                data-tooltip-id={`active_${u.user._id}`}
+                data-tooltip-content={`${user.name} is ${
+                  u.isTyping ? "typing" : "active"
+                }`}
+                data-tooltip-place="top-start"
+              >
+                <img
+                  src={`${import.meta.env.VITE_API_URL}/user/${
+                    u.user._id
+                  }/avatar`}
+                  alt={`Avatar of user ${user.name}`}
+                />
+                {u.isTyping && <small>is typing...</small>}
+              </div>
+              <Tooltip id={`active_${u.user._id}`} />
+            </Fragment>
+          );
+        })}
     </div>
   );
 }
