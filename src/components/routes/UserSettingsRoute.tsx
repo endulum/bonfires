9ff85d42068bccd9af80edd51df089e5d4b1ba12
endulum <71853916+endulum@users.noreply.github.com>
2025-02-27 20:08:14 -0800

    add user settings form

--- src/components/routes/UserSettingsRoute.tsx
+++ src/components/routes/UserSettingsRoute.tsx
@@ -1,14 +1,18 @@
 import { useDocumentTitle } from "usehooks-ts";
 
+import { UserSettingsForm } from "../forms/UserSettingsForm";
 import { LogOutButton } from "../unique/etc/LogOutButton";
 
 export function UserSettingsRoute() {
   useDocumentTitle(`Account Settings :: ${import.meta.env.VITE_APP_NAME}`);
   return (
     <>
-      <div className="flex-row jcspb">
+      <div className="flex-row jcspb mb-1">
         <h2>Account Settings</h2>
         <LogOutButton />
+      </div>
+      <div style={{ overflow: "auto" }}>
+        <UserSettingsForm />
       </div>
     </>
   );
