5ca06aa33ca44f735d8cee81c1c0d9377f8db95f
endulum <71853916+endulum@users.noreply.github.com>
2025-03-01 19:34:07 -0800

    add color to auth card

--- src/components/routes/wrappers/AuthWrapper.tsx
+++ src/components/routes/wrappers/AuthWrapper.tsx
@@ -1,4 +1,7 @@
 import { Outlet } from "react-router-dom";
+import { SvgIcon } from "@mui/material";
+
+import Camp from "../../../assets/icons/camp.svg?react";
 
 export function AuthWrapper({
   context,
@@ -8,8 +11,16 @@ export function AuthWrapper({
   };
 }) {
   return (
-    <div className="auth-main">
-      <Outlet context={context} />
-    </div>
+    <>
+      <h1 className="logo flex-row mb-1">
+        <SvgIcon component={Camp} />
+        <span>Bonfires</span>
+      </h1>
+      <div className="auth">
+        <div className="auth-inner p-1">
+          <Outlet context={context} />
+        </div>
+      </div>
+    </>
   );
 }
